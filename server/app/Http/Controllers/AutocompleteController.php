36f05f1464d6803ed03f0902d8402048bd21a8b8
victorrogov14 <victorrv141004@gmail.com>
2026-06-30 17:42:55 +0300

    Added working prototype of AutocompleteController handling the api/import_replacements route

---
+++ server/app/Http/Controllers/AutocompleteController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ImportReplacement;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AutocompleteController extends Controller
+{
+    public function importReplacements(Request $request): JsonResponse
+    {
+        $query = $this->normalizeQuery($request->query('query', ''));
+
+        if (mb_strlen($query) < 2) {
+            return response()->json([]);
+        }
+
+        $search = mb_strtolower($query, 'UTF-8');
+        $like = '%'.$this->escapeLike($search).'%';
+        $prefixLike = $this->escapeLike($search).'%';
+
+        $matches = ImportReplacement::query()
+            ->select('foreign_product_name')
+            ->whereRaw('LOWER(foreign_product_name) LIKE ? ESCAPE "\\\\"', [$like])
+            ->distinct()
+            ->orderByRaw(
+                '
+                CASE
+                    WHEN LOWER(foreign_product_name) = ? THEN 0
+                    WHEN LOWER(foreign_product_name) LIKE ? ESCAPE "\\\\" THEN 1
+                    ELSE 2
+                END
+                ',
+                [$search, $prefixLike]
+            )
+            ->orderByRaw('LOCATE(?, LOWER(foreign_product_name))', [$search])
+            ->orderByRaw('CHAR_LENGTH(foreign_product_name)')
+            ->orderBy('foreign_product_name')
+            ->limit(5)
+            ->pluck('foreign_product_name')
+            ->map(fn (string $name) => [
+                'label' => $name,
+                'value' => $name,
+            ])
+            ->values();
+
+        return response()->json($matches);
+    }
+
+    private function normalizeQuery(mixed $query): string
+    {
+        return trim(preg_replace('/\s+/u', ' ', (string) $query));
+    }
+
+    private function escapeLike(string $value): string
+    {
+        return str_replace(
+            ['\\', '%', '_'],
+            ['\\\\', '\\%', '\\_'],
+            $value
+        );
+    }
+}
