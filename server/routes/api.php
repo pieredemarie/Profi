10b3b399af2f7f737f191d1e79d729faac5373f6
victorrogov14 <victorrv141004@gmail.com>
2026-07-02 16:35:02 +0300

    Implemented SoftwareClassDisplayController

--- server/routes/api.php
+++ server/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutocompleteController;
+use App\Http\Controllers\SoftwareClassDisplayController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/import_replacements',[AutocompleteController::class, 'importReplacements']);
+Route::get('/import_replacements/software-classes', [SoftwareClassDisplayController::class, 'softwareClasses']);
