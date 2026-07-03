ccbcf30b94290845453bb9fc5003459a004f6392
Khachatryan Artem <65036324+pieredemarie@users.noreply.github.com>
2026-07-03 11:31:23 +0300

    Added: -react-router - new page for results - main page - search bar for result page Changed: - header (added prop for hiding shadow)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,17 @@
-import {Header} from "./components/layout/Header/Header.tsx";
-import {BlueBanner} from "./components/layout/BlueBanner/BlueBanner.tsx";
-import {Footer} from "./components/layout/Footer/Footer.tsx";
-import {Search} from "./components/layout/SearchBar/Search.tsx";
-import {SoftwareTypesSection} from "./components/layout/SoftwareTypesSection/SoftwareTypesSection.tsx";
+import {MainPage} from "./pages/MainPage.tsx";
+import {ResultsPage} from "./pages/ResultsPage.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 function App() {
 
   return (
-    <>
-      <Header/>
-
-        <Search/>
-        <SoftwareTypesSection/>
-        <BlueBanner/>
-      <Footer/>
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/results" element={<ResultsPage />} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
